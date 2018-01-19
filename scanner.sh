555077cf3d8d2602bac84d00c10f785dd36daaa5
Yuhui ZHI <yuhuizhi@Mac.local>
2018-01-20 04:11:15 +0800

    capture info

--- scanner.sh
+++ scanner.sh
@@ -2,9 +2,16 @@
 
 
 regex_ip='(2[0-4][0-9]|25[0-5]|1[0-9][0-9]|[1-9]?[0-9])(\.(2[0-4][0-9]|25[0-5]|1[0-9][0-9]|[1-9]?[0-9])){3}'
-#netstat -an | awk -v regex="$regex_ip" 'match($5, regex){print $1 " " $5 " " $6}' #| grep -E "$regex_ip"
-netstat -an | awk -v regex="$regex_ip" '$5 ~ regex{print $1 " " $5 " " $6}' #| grep -E "$regex_ip"
+
+
+
+{ netstat -an -p tcp; netstat -an -p udp; } | awk -v regex="$regex_ip" '$5 ~ regex{print $1, $5, $6}' | sed -E 's/.([^.]*)$/ \1/'
 
 
 
 
+
+
+
+
+# nslookup -type=PTR 209.58.188.104
